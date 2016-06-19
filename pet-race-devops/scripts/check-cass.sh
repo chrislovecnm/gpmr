cc910c00585a01b6f85c81709163865ca890fec0
chrislovecnm <clove@cnmconsulting.net>
2016-06-19 21:28:11 +0000

    updates to Cassandra

--- pet-race-devops/scripts/check-cass.sh
+++ pet-race-devops/scripts/check-cass.sh
@@ -47,11 +47,9 @@ if [ -n "$RESTARTED" ]; then
 fi
 
 if [ -n "$CASS" ]; then
-  NUM_PO=$(echo "$PO" | wc -l)
   EXEC=$(kubectl.sh exec -it cassandra-2 -- nodetool status | grep UN | wc -l)
   echo "=============================="
   echo "CASSANDRA INFO"
-  echo "cass pods up:       $NUM_PO"
   echo "nodetool status UN: $EXEC"
 fi
 
