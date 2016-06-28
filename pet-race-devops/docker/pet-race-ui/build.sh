6a488d7d5f0f7585e6108894fdd8ed5e0bfa7dea
chrislovecnm <clove@cnmconsulting.net>
2016-06-28 09:21:39 +0000

    updates

--- pet-race-devops/docker/pet-race-ui/build.sh
+++ pet-race-devops/docker/pet-race-ui/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=v1.1
+VERSION=v1.3
 
 usage() {
   echo "Usage: $0 [-p]" 1>&2;
@@ -39,7 +39,7 @@ docker pull jhipster/jhipster
 docker stop jhipster
 docker rm jhipster
 docker run --name jhipster -d -t jhipster/jhipster
-docker exec jhipster /bin/bash -c 'git clone https://github.com/k8s-for-greeks/gpmr.git . && cd pet-race-ui && npm install && ./gradlew bootRepackage -PskipTest -Pprod -x test -x gulp_test && mv build/libs/*.war ~/app.war'
+docker exec jhipster /bin/bash -c 'git clone https://github.com/k8s-for-greeks/gpmr.git . && cd pet-race-ui && npm install && npm install statsjs && bower install && ./gradlew bootRepackage -PskipTest -Pprod -x test -x gulp_test && mv build/libs/*.war ~/app.war'
 docker cp jhipster:/home/jhipster/app.war $PWD/
 docker stop jhipster
 docker rm jhipster
